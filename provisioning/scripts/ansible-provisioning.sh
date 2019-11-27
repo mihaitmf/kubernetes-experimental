0c6118340c070c0ec1289674088846ad05fcf2fb
mihaitmf <mihai.tmf@gmail.com>
2019-11-27 13:13:21 +0200

    Fix "set-working-directory" command

--- provisioning/scripts/ansible-provisioning.sh
+++ provisioning/scripts/ansible-provisioning.sh
@@ -4,7 +4,7 @@
 /vagrant/provisioning/scripts/install-ansible.sh
 
 # Set working directory for future logins
-grep -Pq 'cd /vagrant/provisioning/ansible' $HOME/.bashrc || echo -e "\ncd /vagrant/provisioning/ansible\n" >> $HOME/.bashrc
+grep -Pq 'cd /vagrant/provisioning/ansible' /home/vagrant/.bashrc || echo -e "\ncd /vagrant/provisioning/ansible\n" >> /home/vagrant/.bashrc
 
 # Provision the rest of the machines defined in the "hosts" inventory file with Ansible
 cd /vagrant/provisioning/ansible
