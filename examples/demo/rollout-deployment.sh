bfe4304d84391fb2788c16c0b401da84977dc253
mihaitmf <mihai.tmf@gmail.com>
2019-12-18 20:40:43 +0200

    Add commands for kubectl rollout

---
+++ examples/demo/rollout-deployment.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+kubectl create -f deployment-definition.yml --record
+
+kubectl get all -o wide
+kubectl rollout status deployment/myapp-deployment
+
+# Modify the replicas property in the definition file
+sed -i 's/image: nginx/image: nginx:1.12/g' deployment-definition.yml
+kubectl apply -f deployment-definition.yml --record
+
+kubectl rollout status deployment/myapp-deployment
+kubectl rollout history deployment/myapp-deployment
+kubectl get pods -o wide
+kubectl describe deployment
+
+kubectl rollout undo deployment/myapp-deployment
+
+kubectl rollout status deployment/myapp-deployment
+kubectl rollout history deployment/myapp-deployment
+kubectl get pods -o wide
