325228b15105efda774e6c5baeeba18c14ca59bf
mihaitmf <mihai.tmf@gmail.com>
2019-12-18 19:53:52 +0200

    Add comment regarding the "kubectl get all -v=99" command

--- examples/demo/create-deployment.sh
+++ examples/demo/create-deployment.sh
@@ -5,5 +5,8 @@ kubectl get deployment -o wide
 kubectl get replicaset -o wide
 kubectl get pods -o wide
 
+# To debug in case the "get all" command returns very slowly add the "-v=99" argument
+#kubectl get all -v=99
+
 # To remove the POD run:
 #kubectl delete -f deployment-definition.yml
